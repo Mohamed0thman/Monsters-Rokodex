9514690520b296f5e3899328b3d3c045ad3de6b8
Mikky <mahmoudothman.dev@gmail.com>
2020-03-18 04:22:06 +0200

    Updates

---
+++ precache-manifest.b7be454f9e720ea888abf3def79bd15f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218bf7e40e2b8efeaddb92a21ccd1890",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5f60ff8288a9a249a407",
+    "url": "/monsters-rolodex/static/css/main.9ab15262.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f60ff8288a9a249a407",
+    "url": "/monsters-rolodex/static/js/main.c5b276e7.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
